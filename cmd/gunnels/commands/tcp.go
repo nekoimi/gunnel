3a1e1f2632a791f3587431af9e8163e109fc57c0
nekoimi <yyprisoner@gmail.com>
2020-10-27 23:06:28 +0800

    up

--- cmd/gunnels/commands/tcp.go
+++ cmd/gunnels/commands/tcp.go
@@ -1,1 +1,15 @@
 package commands
+
+import "github.com/spf13/cobra"
+
+var tcpSrvCmd = &cobra.Command{
+	Use:   "tcp",
+	Short: "",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(tcpSrvCmd)
+}
--- config/config.go
+++ config/config.go
@@ -1,1 +1,4 @@
 package config
+
+type ServerConfig struct {
+}
